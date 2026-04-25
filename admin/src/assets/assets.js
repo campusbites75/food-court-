160e22d464e83977d8ecf9cc777ca6fc88110663
campusbites75 <campusbites75@gmail.com>
2026-04-25 21:30:58 +0530

    Add files via upload

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,17 @@
+import logo from "./logo.png";
+import order_icon from "./order_icon.png";
+import parcel_icon from "./parcel_icon.png";
+import profile_image from "./profile_image.png";
+import upload_area from "./upload_area.png";
+
+// 🔥 ADD THESE
+export const url = "http://localhost:4000"; // backend URL
+export const currency = "₹"; // or "$"
+
+export const assets = {
+  logo,
+  order_icon,
+  parcel_icon,
+  profile_image,
+  upload_area,
+};
